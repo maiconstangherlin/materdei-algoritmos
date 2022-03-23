07694e0b04cbfac943aff6edac63ef33563eebbc
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2022-03-22 23:35:42 -0300

    Declarando variáveis

---
+++ src/main/java/br/edu/materdei/algoritmos/variaveis/Declaracao.java
@@ -0,0 +1,22 @@
+package br.edu.materdei.algoritmos.variaveis;
+
+public class Declaracao {
+
+    public static void main(String[] args) {
+        
+        // Sintaxe de declaração recebendo um valor
+        // tipo | nome = valor;
+        int numero = 1;         
+        
+        
+        
+        
+        // Sintaxe de declaração sem passar valor inicial
+        // tipo | nome;
+        String texto;
+        
+        // atribuindo valor após a criação
+        // nome = valor;
+        texto = "Hello World";        
+    }
+}
