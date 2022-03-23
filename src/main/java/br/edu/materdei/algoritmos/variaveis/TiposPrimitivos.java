f578b7a5cae621405c54ea11aae1b6197eeb6cf7
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2022-03-22 23:27:36 -0300

    Tipos primitivos

---
+++ src/main/java/br/edu/materdei/algoritmos/variaveis/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package br.edu.materdei.algoritmos.variaveis;
+
+public class TiposPrimitivos {
+    
+    public static void main(String[] args) {
+        
+        byte numByte = -128; // -128 até 127
+        short numShort = 32767; // -32768 até 32767
+        int numInt = 123456789; // -2³¹ <-> 2³¹
+        long numLong = 987654321; // -2⁶⁴ <-> 2⁶⁴
+        
+        float numFloat = 123456.123456f; // -10³⁸ <-> 10³⁸
+        double numDouble = 9999999.99999999; //-10³⁰⁸ <-> 10³⁰⁸
+
+        boolean valueBoolean = true; // true ou false
+        
+        char valueChar = 'a'; //qualquer caractere único (entre aspas simples)
+        
+        
+    }
+    
+}
